f3ff47879aac3f463c389d39df3509d0dd62b398
Vítor Fernandes <VitorResendeFernandes+GitHub@gmail.com>
2024-07-06 13:17:33 -0400

    Remove inline to fix linking errors

--- source/mos6502/cpuio.h
+++ source/mos6502/cpuio.h
@@ -5,10 +5,10 @@
 struct CPU;
 struct Bus;
 
-inline uint8_t Read(struct CPU* cpu);
-inline void Write(struct CPU* cpu, uint8_t data);
+uint8_t Read(struct CPU* cpu);
+void Write(struct CPU* cpu, uint8_t data);
 
-inline uint8_t Pop(struct CPU* cpu);
-inline void Push(struct CPU* cpu, uint8_t data);
+uint8_t Pop(struct CPU* cpu);
+void Push(struct CPU* cpu, uint8_t data);
 
-inline uint16_t ReadAddress(struct CPU* cpu);
+uint16_t ReadAddress(struct CPU* cpu);
