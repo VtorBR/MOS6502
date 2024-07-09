3a2dcb953acf7b24db0b35fce281d885cf1fc5f6
Vítor Fernandes <VitorResendeFernandes+GitHub@gmail.com>
2024-07-09 19:10:04 -0400

    Add implied addressing operations

--- source/mos6502/operation.c
+++ source/mos6502/operation.c
@@ -1,6 +1,7 @@
 #include "mos6502/operation.h"
 
 #include "mos6502/cpu.h"
+#include "mos6502/cpuio.h"
 #include "mos6502/u16.h"
 
 void BRK(struct CPU* cpu)
@@ -15,9 +16,84 @@ void BRK(struct CPU* cpu)
 	*HI(&cpu->programCounter) = Read(cpu);
 }
 
+void CLC(struct CPU* cpu)
+{
+	cpu->status.carry = 0;
+}
+
+void CLD(struct CPU* cpu)
+{
+	cpu->status.decimalMode = 0;
+}
+
+void CLI(struct CPU* cpu)
+{
+	cpu->status.irqDisable = 0;
+}
+
+void CLV(struct CPU* cpu)
+{
+	cpu->status.overflow = 0;
+}
+
+void DEX(struct CPU* cpu)
+{
+	--cpu->xIndex;
+
+	cpu->status.negative = cpu->xIndex & 0x80;
+	cpu->status.zero = cpu->xIndex == 0x00;
+}
+
+void DEY(struct CPU* cpu)
+{
+	--cpu->yIndex;
+
+	cpu->status.negative = cpu->yIndex & 0x80;
+	cpu->status.zero = cpu->yIndex == 0x00;
+}
+
+void INX(struct CPU* cpu)
+{
+	++cpu->xIndex;
+
+	cpu->status.negative = cpu->xIndex & 0x80;
+	cpu->status.zero = cpu->xIndex == 0x00;
+}
+
+void INY(struct CPU* cpu)
+{
+	++cpu->yIndex;
+
+	cpu->status.negative = cpu->yIndex & 0x80;
+	cpu->status.zero = cpu->yIndex == 0x00;
+}
+
 void NOP(struct CPU* cpu)
 {
 
+}
+
+void PHA(struct CPU* cpu)
+{
+	Push(cpu, cpu->accumulator);
+}
+
+void PHP(struct CPU* cpu)
+{
+	Push(cpu, cpu->status.flags);
+}
+
+void PLA(struct CPU* cpu)
+{
+	cpu->accumulator = Pop(cpu);
+
+	cpu->status.negative = cpu->accumulator & 0x80;
+	cpu->status.zero = cpu->accumulator == 0x00;
+}
+
+void PLP(struct CPU* cpu)
+{
+	cpu->status.flags = Pop(cpu);
 }
 
 void RTI(struct CPU* cpu)
@@ -25,4 +101,73 @@ void RTI(struct CPU* cpu)
 	cpu->status.flags = Pop(cpu);
 	*LO(&cpu->programCounter) = Pop(cpu);
 	*HI(&cpu->programCounter) = Pop(cpu);
+}
+
+void RTS(struct CPU* cpu)
+{
+	*LO(&cpu->programCounter) = Pop(cpu);
+	*HI(&cpu->programCounter) = Pop(cpu);
+}
+
+void SEC(struct CPU* cpu)
+{
+	cpu->status.carry = 1;
+}
+
+void SED(struct CPU* cpu)
+{
+	cpu->status.decimalMode = 1;
+}
+
+void SEI(struct CPU* cpu)
+{
+	cpu->status.irqDisable = 1;
+}
+
+void TAX(struct CPU* cpu)
+{
+	cpu->xIndex = cpu->accumulator;
+
+	cpu->status.negative = cpu->accumulator & 0x80;
+	cpu->status.zero = cpu->accumulator == 0x00;
+}
+
+void TAY(struct CPU* cpu)
+{
+	cpu->yIndex = cpu->accumulator;
+
+	cpu->status.negative = cpu->accumulator & 0x80;
+	cpu->status.zero = cpu->accumulator == 0x00;
+}
+
+void TSX(struct CPU* cpu)
+{
+	cpu->xIndex = cpu->stackPointer;
+
+	cpu->status.negative = cpu->xIndex & 0x80;
+	cpu->status.zero = cpu->xIndex == 0x00;
+}
+
+void TXA(struct CPU* cpu)
+{
+	cpu->accumulator = cpu->xIndex;
+
+	cpu->status.negative = cpu->accumulator & 0x80;
+	cpu->status.zero = cpu->accumulator == 0x00;
+}
+
+void TXS(struct CPU* cpu)
+{
+	cpu->stackPointer = cpu->xIndex;
+
+	cpu->status.negative = cpu->xIndex & 0x80;
+	cpu->status.zero = cpu->xIndex == 0x00;
+}
+
+void TYA(struct CPU* cpu)
+{
+	cpu->accumulator = cpu->yIndex;
+
+	cpu->status.negative = cpu->accumulator & 0x80;
+	cpu->status.zero = cpu->accumulator == 0x00;
 }
--- source/mos6502/operation.h
+++ source/mos6502/operation.h
@@ -3,5 +3,27 @@
 struct CPU;
 
 void BRK(struct CPU*);
+void CLC(struct CPU*);
+void CLD(struct CPU*);
+void CLI(struct CPU*);
+void CLV(struct CPU*);
+void DEX(struct CPU*);
+void DEY(struct CPU*);
+void INX(struct CPU*);
+void INY(struct CPU*);
 void NOP(struct CPU*);
+void PHA(struct CPU*);
+void PHP(struct CPU*);
+void PLA(struct CPU*);
+void PLP(struct CPU*);
 void RTI(struct CPU*);
+void RTS(struct CPU*);
+void SEC(struct CPU*);
+void SED(struct CPU*);
+void SEI(struct CPU*);
+void TAX(struct CPU*);
+void TAY(struct CPU*);
+void TSX(struct CPU*);
+void TXA(struct CPU*);
+void TXS(struct CPU*);
+void TYA(struct CPU*);
