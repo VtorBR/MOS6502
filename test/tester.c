12cfb55f38e73e749b843726ef21513575b3428b
Vítor Fernandes <VitorResendeFernandes+GitHub@gmail.com>
2024-08-18 11:07:28 -0400

    Replace spaces with tabs

--- test/tester.c
+++ test/tester.c
@@ -9,24 +9,24 @@
 
 uint8_t ReadMemory(void* memory, uint16_t address)
 {
-    return ((uint8_t*)memory)[address];
+	return ((uint8_t*)memory)[address];
 }
 
 void WriteMemory(void* memory, uint16_t address, uint8_t data)
 {
-    ((uint8_t*)memory)[address] = data;
+	((uint8_t*)memory)[address] = data;
 }
 
 int main(int argc, char** argv)
 {
-    if (argc < 2)
-    {
-        fprintf(stderr, "Usage: %s rom_file [max_cycle_count] [starting_address]\n", argv[0]);
+	if (argc < 2)
+	{
+		fprintf(stderr, "Usage: %s rom_file [max_cycle_count] [starting_address]\n", argv[0]);
 
-        return EXIT_FAILURE;
-    }
+		return EXIT_FAILURE;
+	}
 
-    uint8_t* memory = malloc(1 << 16);
+	uint8_t* memory = malloc(1 << 16);
 
 	FILE* rom = fopen(argv[1], "rb");
 	if (rom)
@@ -34,14 +34,14 @@ int main(int argc, char** argv)
 		fread(memory, 1, 1 << 16, rom);
 		fclose(rom);
 	}
-    else
-    {
-        fprintf(stderr, "Could not open ROM file at %s\n", argv[1]);
+	else
+	{
+		fprintf(stderr, "Could not open ROM file at %s\n", argv[1]);
 
-        return EXIT_FAILURE;
-    }
+		return EXIT_FAILURE;
+	}
 
-    struct Bus bus =
+	struct Bus bus =
 	{
 		.Read = ReadMemory,
 		.Write = WriteMemory,
@@ -53,47 +53,47 @@ int main(int argc, char** argv)
 		.bus = &bus
 	};
 
-    uint64_t maxCycleCount = 1e8;
+	uint64_t maxCycleCount = 1e8;
 
-    if (argc > 2)
-    {
-        maxCycleCount = atoi(argv[2]);
-    }
+	if (argc > 2)
+	{
+		maxCycleCount = atoi(argv[2]);
+	}
 
-    if (argc > 3)
-    {
-        cpu.programCounter = atoi(argv[3]);
-    }
-    else
-    {
-        Reset(&cpu);
-    }
+	if (argc > 3)
+	{
+		cpu.programCounter = atoi(argv[3]);
+	}
+	else
+	{
+		Reset(&cpu);
+	}
 
 	uint64_t cycleCount = 0;
-    uint16_t lastInstruction = cpu.programCounter;
+	uint16_t lastInstruction = cpu.programCounter;
 
 	while (cycleCount++ < maxCycleCount)
 	{
-        Clock(&cpu);
+		Clock(&cpu);
 
-        if (cpu.internal.cycles == 0)
-        {
-            if (lastInstruction == cpu.programCounter)
-            {
-                char assembly[13];
-			    Disassemble(memory + cpu.programCounter, assembly);
-                fprintf(stderr, "Trapped at %04X : %-12s\n", cpu.programCounter, assembly);
+		if (cpu.internal.cycles == 0)
+		{
+			if (lastInstruction == cpu.programCounter)
+			{
+				char assembly[13];
+				Disassemble(memory + cpu.programCounter, assembly);
+				fprintf(stderr, "Trapped at %04X : %-12s\n", cpu.programCounter, assembly);
 
-                return cpu.programCounter;
-            }
+				return cpu.programCounter;
+			}
 
-            lastInstruction = cpu.programCounter;
-        }
+			lastInstruction = cpu.programCounter;
+		}
 	}
 
 	free(memory);
-    
-    fprintf(stderr, "Exceeded max cycle count: %llu\n", maxCycleCount);
+	
+	fprintf(stderr, "Exceeded max cycle count: %llu\n", maxCycleCount);
 
-    return EXIT_FAILURE;
+	return EXIT_FAILURE;
 }
